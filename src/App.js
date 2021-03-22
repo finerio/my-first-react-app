ef8719c86e57bcd7fa9f03db467e22ac34593659
finerio <ariel.shadmon@gmail.com>
2021-03-22 21:27:41 +0100

    using map with components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,54 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
+
+const all_pokemon = [
+  {
+    name: "Charizard",
+    weight: 90,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Blaze", "Solar power", "Tough claws", "Drought"],
+  },
+  {
+    name: "Bulbasaur",
+    weight: 6.9,
+    awesome: true,
+    terrifying: false,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Mewtwo",
+    weight: 122,
+    awesome: true,
+    terrifying: true,
+    abilities: ["Pressure", "Unnerve", "Steadfast", "Insomnia"],
+  },
+  {
+    name: "Mega beedrill",
+    weight: 65,
+    awesome: false,
+    terrifying: true,
+    abilities: ["Intimidate", "Unnerve"],
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <main>
         <Title content="Some Simple Title" /> <h1>Hello World!</h1>
+        {all_pokemon.map((pokemon, index) => (
+          <Pokemon
+            key={index}
+            name={pokemon.name}
+            weight={pokemon.weight}
+            awesome={pokemon.awesome}
+            terrifying={pokemon.terrifying}
+            abilities={pokemon.abilities}
+          />
+        ))}{" "}
         <p>React is cool!</p>
       </main>
 
