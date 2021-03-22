dd36a1814e1b0534ed23a9818f64f8b975828cb8
finerio <ariel.shadmon@gmail.com>
2021-03-22 12:15:57 +0100

    Welcome to Codaisseur3

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Welcome to Codaisseur
         </a>
       </header>
     </div>
